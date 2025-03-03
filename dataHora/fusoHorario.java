0fc5c6b22feefbe31d8f36d24ea70387c025fe9c
gustavo portela <portelagustavo947@gmail.com>
2025-03-03 17:19:36 -0300

    03/03 fusoHorario 50% completed

---
+++ dataHora/fusoHorario.java
@@ -0,0 +1,23 @@
+package dataHora;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class fusoHorario {
+    public static void main(String[] args) {
+
+        LocalDate d01 = LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now();
+        LocalDate d04 = LocalDate.parse("2022-07-20T01:30:36");
+        LocalDateTime d05 = LocalDateTime.parse("")
+        System.out.println(d01);
+        System.out.println(d02);
+        System.out.println(d03);
+        System.out.println(d04);
+
+    }
+
+
+}
