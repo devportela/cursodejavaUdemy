816ec85af67036fe516d37a473933eebca0854f8
gustavo portela <portelagustavo947@gmail.com>
2025-04-06 10:18:45 -0300

    polimorfimso intial

---
+++ polimorfismo/program.java
@@ -0,0 +1,16 @@
+package polimorfismo;
+
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number of employes");
+        
+
+
+
+        sc.close();
+
+    }
+}
