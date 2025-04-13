2f7bb4fd64018705b9362f2d01846ae672602cb0
gustavo portela <portelagustavo947@gmail.com>
2025-04-13 13:29:30 -0300

    try catch end

--- excecoes/tryCatch.java
+++ excecoes/tryCatch.java
@@ -1,15 +1,25 @@
 package excecoes;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class tryCatch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String[] vect = sc.nextLine().split("");
-        int position = sc.nextInt();
-        System.out.println(vect[position]);
+        try {
+            String[] vect = sc.nextLine().split("");
 
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("invalid position!"); //tratamento de erro de posição
+        } catch (InputMismatchException e) {
+            System.out.println("input error!"); //tratamento de erro de inserção de letra na posicao do vetor
+        }
+
+
+        System.out.println("end of program...");//agora essa mensagem é vista ,pois o codigo esta tratando os erros que podem aconteceer na execução
         sc.close();
     }
 }
