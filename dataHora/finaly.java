0b5e2cee33dc96015e2e9728bf014efc2287517f
gustavo portela <portelagustavo947@gmail.com>
2025-04-13 16:22:30 -0300

    bloco finally edn

---
+++ dataHora/finaly.java
@@ -0,0 +1,29 @@
+package dataHora;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class finaly{ //arquivo que usa files e verifica o arquivo in.txt e imprime na tela o conteudo do mesmo
+    //será estudado mais pra frente no curso //
+
+
+    public static void main(String[] args) {
+        File file = new File("C:\\temp\\in.txt.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error opening file: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+            System.out.println("finally block executed");
+        }
+    }
+}
+
