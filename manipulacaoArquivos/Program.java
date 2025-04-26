af52a4ca0032516764420fc6ee0d8a8d51a38324
gustavo portela <portelagustavo947@gmail.com>
2025-04-26 08:25:15 -0300

    manipulacao de arquivos - leitura com classes file e scanner

---
+++ manipulacaoArquivos/Program.java
@@ -0,0 +1,29 @@
+package manipulacaoArquivos;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) { //codigo para ler o conteudo do arquivo teste.txt e mostrar na tela
+
+        File file = new File("C:\\temp\\teste.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) ;{
+
+                System.out.println(sc.hasNextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            if(sc != null) {
+                sc.close();
+            }
+        }
+
+    }
+}
