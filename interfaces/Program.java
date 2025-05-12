ee58f104769b8ee0ea5474f9f1f119c14c2179a9
gustavo portela <portelagustavo947@gmail.com>
2025-05-12 11:48:11 -0300

    solucao problema pt2

--- interfaces/Program.java
+++ interfaces/Program.java
@@ -19,8 +19,25 @@ public class Program {
         System.out.println("retorno (dd/MM/yyyy hh:mm): ");
         LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 
-        CarRental cr = new CarRental(start,finish,new Vehicle(carModel));
+        CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
 
+        System.out.println("entre com o preço por hora: ");
+        double pricePerHour = sc.nextDouble();
+        System.out.println("entre com o preço por dia: ");
+        double pricePerDay = sc.nextDouble();
+
+
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrasilTaxService());
+
+        rentalService.processInvoice(cr);
+
+        System.out.println("FATURA: ");
+        System.out.println("pagamento basico: " + cr.getInvoice().getBasicPayment());
+        System.out.println("imposto: " + cr.getInvoice().getTax());
+        System.out.println("pagamento total:" + cr.getInvoice().getTotalPayment());
+
+
+        sc.close();
 
     }
 }
